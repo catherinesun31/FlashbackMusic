06e5660db798e03040fa3b9b500b45dcaa08a7a0
cailintreseder <ctresede@ucsd.edu>
2018-02-07 09:35:09 -0800

    Trying again

--- app/src/main/java/com/android/flashbackmusic/SongsList.java
+++ app/src/main/java/com/android/flashbackmusic/SongsList.java
@@ -37,7 +37,9 @@ public class SongsList extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
+        // Handle actgit git
+        //
+        //
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
