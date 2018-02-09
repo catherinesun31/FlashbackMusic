93b4536446d33d8c052f65300e0eccb5907de4f5
cailintreseder <ctresede@ucsd.edu>
2018-02-09 09:33:04 -0800

    Finished UI for Song List, no APK error with emulator.

--- app/src/main/java/com/android/flashbackmusicv000/SongListActivity.java
+++ app/src/main/java/com/android/flashbackmusicv000/SongListActivity.java
@@ -14,7 +14,7 @@ public class SongListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_song_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
