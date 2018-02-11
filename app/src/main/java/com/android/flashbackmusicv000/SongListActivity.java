aaf79c9cefaee5725e34f3ac1512cf7eebba62fb
cailintreseder <ctresede@ucsd.edu>
2018-02-10 20:21:16 -0800

    Dynamically creates song buttons from list of files in R.raw

--- app/src/main/java/com/android/flashbackmusicv000/SongListActivity.java
+++ app/src/main/java/com/android/flashbackmusicv000/SongListActivity.java
@@ -4,20 +4,27 @@ import android.content.res.AssetFileDescriptor;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ScrollView;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
 public class SongListActivity extends AppCompatActivity {
 
@@ -32,6 +39,7 @@ public class SongListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         int totalSongs = getNumberOfSongs();
+        //Log.d("Total Songs", totalSongs + "");
         MEDIA_RES_IDS = new int[totalSongs];
         /*
         AssetFileDescriptor assetFileDescriptor = this.getResources().openRawResourceFd(MEDIA_RES_ID);
@@ -42,55 +50,100 @@ public class SongListActivity extends AppCompatActivity {
         catch (Exception e) {
             System.out.println(e.toString());
         }
+        */
+
+        Log.d("Filepath", Environment.getExternalStorageDirectory().toString());
 
         java.io.File file = new java.io.File("/Users/cailintreseder/AndroidStudioProjects/FlashbackMusic/app/src/main/res/raw");
         File[] files = file.listFiles();
-        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
-        Button songButton;
+        android.support.constraint.ConstraintLayout constraintLayout =
+                (android.support.constraint.ConstraintLayout)findViewById(R.id.constraintLayout);
+        //Button songButton;
+        Log.d("To beginning of for loop", "To beginning of for loop");
+        Field[] fields = R.raw.class.getFields();
+        final float scale = this.getResources().getDisplayMetrics().density;
+        int songId = fields[0].getName().replace(".mp3", "").hashCode();
+        int pixels = (int) (50 * scale + 0.5f);
+        int textSize = (int) (15 * scale + 0.5f);
+        int buttonId = songId + 1;
 
-        for (int i = 0; i < totalSongs; ++i) {
-            String fileName = files[i].getName().replace(".mp3", "");
+        for (int i = 0; i < fields.length; ++i) {
+            String fileName = fields[i].getName().replace(".mp3", "");
             Button button = new Button(this);
-            ScrollView.LayoutParams params = new ScrollView.LayoutParams(
-                    ScrollView.LayoutParams.MATCH_PARENT,
-                    50,
-                    Gravity.CENTER_VERTICAL
-            );
-            button.setId(button.toString().hashCode());
+            android.support.constraint.ConstraintLayout.LayoutParams params = new
+                    android.support.constraint.ConstraintLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT, pixels);
+            button.setId(fileName.toString().hashCode());
             button.setText(fileName);
             button.setBackgroundColor(Color.rgb(230, 230, 230));
-            button.setTextAlignment(View.TEXT_ALIGNMENT_GRAVITY);
+            button.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
             button.setTextColor(Color.rgb(89,89,89));
-            button.setTextSize(20f);
+            button.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
             button.setSingleLine(true);
             button.setEllipsize(TextUtils.TruncateAt.MARQUEE);
             button.setMarqueeRepeatLimit(1000);
-            scrollView.addView(button, params);
-            songButton = (Button) findViewById(button.getId());
-            songButton.setOnClickListener(new View.OnClickListener() {
+            constraintLayout.addView(button, params);
+
+            /*button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     //set button listener
                 }
-            });
+            });*/
 
             Button add = new Button(this);
-            ScrollView.LayoutParams smallParams = new ScrollView.LayoutParams(
-                    50,
-                    50
-            );
-            add.setId(add.toString().hashCode());
-            add.setText("@string/favorited");
+            android.support.constraint.ConstraintLayout.LayoutParams smallParams = new
+                    android.support.constraint.ConstraintLayout.LayoutParams(
+                    pixels, pixels);
+            add.setId((int)button.getId() + 1);
+            add.setText("✓");
             add.setBackgroundColor(Color.rgb(230, 230, 230));
-            add.setTextSize(20f);
-            scrollView.addView(add, smallParams);
-            add = (Button) findViewById(add.getId());
-            add.setOnClickListener(new View.OnClickListener() {
+            add.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize);
+            constraintLayout.addView(add, smallParams);
+            //add = (Button) findViewById(add.getId());
+            /*add.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     //set button listener
                 }
-            });
+            });*/
+
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(
+                    button.getId(), ConstraintSet.LEFT,
+                    ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+            constraintSet.connect(
+                    add.getId(), ConstraintSet.RIGHT,
+                    ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+            constraintSet.connect(
+                    button.getId(), ConstraintSet.END,
+                    add.getId(), ConstraintSet.START, pixels);
+
+
+            if (i == 0) {
+                constraintSet.connect(
+                        button.getId(), ConstraintSet.TOP,
+                        ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
+                constraintSet.connect(
+                        add.getId(), ConstraintSet.TOP,
+                        ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
+            }
+
+            else {
+                constraintSet.connect(
+                        button.getId(), ConstraintSet.TOP,
+                        songId, ConstraintSet.BOTTOM, 0);
+                constraintSet.connect(
+                        add.getId(), ConstraintSet.TOP,
+                        buttonId, ConstraintSet.BOTTOM, 0);
+            }
+            constraintSet.constrainDefaultHeight(button.getId(), pixels);
+            constraintSet.constrainDefaultHeight(add.getId(), pixels);
+            constraintSet.applyTo(constraintLayout);
+
+            songId = button.getId();
+            buttonId = songId + 1;
         }
-        */
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
