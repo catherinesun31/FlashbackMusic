606dc49f52d698f9ea7ba1c3ce837a65f02400f8
Brigid <bcoverto@ucsd.edu>
2018-02-18 13:05:21 -0800

    flashbackmode.java

---
+++ app/src/main/java/com/android/flashbackmusicv000/FlashBackMode.java
@@ -0,0 +1,26 @@
+package com.android.flashbackmusicv000;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Brigid on 2/17/18.
+ */
+
+public class FlashBackMode {
+    ArrayList<Song> flashQueue;
+
+
+
+    public void turnFlashBackModeOn() {
+
+    }
+
+    public void createQueue() {
+
+
+
+    }
+
+
+
+}
