8b7c1713f02f08742b1a39bcafb113b6a0d75211
cailintreseder <ctresede@ucsd.edu>
2018-02-18 16:23:09 -0800

    Gets current location whenever a song is played, almost to getting consistent location updates

--- app/src/main/java/com/android/flashbackmusicv000/SongPlayingActivity.java
+++ app/src/main/java/com/android/flashbackmusicv000/SongPlayingActivity.java
@@ -1,6 +1,8 @@
 package com.android.flashbackmusicv000;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
@@ -11,28 +13,28 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
-import android.support.design.widget.FloatingActionButton;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -40,24 +42,37 @@ import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class SongPlayingActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private MediaPlayer mediaPlayer;
-    private FusedLocationProviderClient mFusedLocationClient;
-    private AddressResultReceiver mResultReceiver;
-    private Location locationManager;
+public class SongPlayingActivity extends AppCompatActivity implements
+        OnMapReadyCallback {
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        //do nothing
-    }
+    private MediaPlayer mediaPlayer;
+    Context mContext;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location locationManager;
+    LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    private LocationCallback mLocationCallback;
+    private boolean mRequestingLocationUpdates;
+    final String REQUESTING_LOCATION_UPDATES_KEY = "Requesting Location Updates";
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private AddressResultReceiver mResultReceiver;
+
+    protected String mAddressOutput;
+    protected String mAreaOutput;
+    protected String mCityOutput;
+    protected String mStateOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        Log.i("In: ", "SongPlayingActivity.onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_playing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mContext = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +92,13 @@ public class SongPlayingActivity extends AppCompatActivity implements OnMapReady
         final int nameInt = song.getSongId();
         loadMedia(nameInt);
         mediaPlayer.start();
-        getLocation(savedInstanceState);
+
         song.setTime(getTime());
         song.setDate(getDate());
         song.setDay(getDay());
+
+        mLocationCallback = new LocationCallback();
+        song.setLocation(getLocation());
 
         TextView songTime = (TextView) findViewById(R.id.song_time);
         String time = song.getLastDay() + " " + song.getLastDate() + " " + song.getLastTime();
@@ -109,8 +127,68 @@ public class SongPlayingActivity extends AppCompatActivity implements OnMapReady
                 }
             }
         });
+        mResultReceiver = new AddressResultReceiver(new Handler());
+
     }
 
+
+
+    @Override
+    protected void onStop() {
+        Log.i("In: ", "SongPlayingActivity.onStop");
+        super.onStop();
+        try {
+
+        }
+        catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.i("In: ", "SongPlayingActivity.onMapReady");
+
+        Log.d("MAP LOCATION", "OnMapReady");
+        mMap = googleMap;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i("In: ", "SongPlayingActivity.onSaveInstanceState");
+
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        // ...
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy(){
+        Log.i("In: ", "SongPlayingActivity.onDestroy");
+
+        super.onDestroy();
+        mediaPlayer.release();
+    }
+
+    private void changeMap(Location location) {
+        Log.i("In: ", "SongPlayingActivity.changeMap");
+
+        Log.d("MAP LOCATION", "Reaching map" + mMap);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+    }
+
+
+    //TODO (if enough time): change this to a Time class, so that it's SRP (but also who cares)
     private String getTime() {
         Calendar calendar = Calendar.getInstance();
         Date currentTime = calendar.getTime();
@@ -135,51 +213,72 @@ public class SongPlayingActivity extends AppCompatActivity implements OnMapReady
         return dateFormat.format(currentTime);
     }
 
-    private void getLocation(Bundle savedInstanceState) {
+    //TODO (if enough time): change this to a Location class, so that it's SRP (but also who cares)
+    private String getLocation() {
+        Log.i("In: ", "SongPlayingActivity.getLocation");
 
-        //Get the user's location
+        String address = "";
+
+        //Ask for location permissions
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     100);
-            Log.d("test1","ins");
         }
 
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+        //Get the user's location
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 //logic to handle location
                 if (location != null) {
                     locationManager = location;
+
                     Log.d("Current Location", "Longitude: " + locationManager.getLongitude() + "\n"
                             + "Latitude: " + locationManager.getLatitude());
-                    //Add the location to the song's last-played location
-                    String cityName=null;
-                    Geocoder gcd = new Geocoder(getBaseContext(), Locale.US);
-                    List<Address> addresses;
-                    String StreetName = "";
-                    double longitude = locationManager.getLongitude();
-                    double latitude = locationManager.getLatitude();
-                    try {
-                        addresses = gcd.getFromLocation(latitude, longitude, 1);
-                        if (addresses.size() > 0) StreetName = addresses.get(0).getThoroughfare();
-                        String s = longitude + "\n" + latitude +
-                                "\n\nMy Currrent Street is: " + StreetName;
-                        Log.d("Location", s);
-                    }
-                    catch (Exception e) {
-                        Log.d("Exception", "There was an error with the Geocoder");
-                    }
+
+                    //Get the location as an address
+
+                    //Log.d("Address: ", mAddressOutput);
                 }
             }
         });
 
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        //logic to handle location
+                        if (location != null) {
+                            locationManager = location;
+
+                            Log.d("Current Location", "Longitude: " + locationManager.getLongitude() + "\n"
+                                    + "Latitude: " + locationManager.getLatitude());
+
+                            //Get the location as an address
+                            startIntentService(locationManager);
+                        }
+                    }
+                });
+        return address;
 
     }
 
+    protected void startIntentService(Location location) {
+        Log.i("In: ", "SongPlayingActivity.startIntentService");
+
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(FetchAddressIntentService.Constants.RECEIVER, mResultReceiver);
+        intent.putExtra(FetchAddressIntentService.Constants.LOCATION_DATA_EXTRA, locationManager);
+        startService(intent);
+    }
+
     public void loadMedia(int resourceId){
+        Log.i("In: ", "SongPlayingActivity.loadMedia");
+
         if (mediaPlayer == null){
             mediaPlayer = new MediaPlayer();
         }
@@ -200,39 +299,4 @@ public class SongPlayingActivity extends AppCompatActivity implements OnMapReady
             System.out.println(e.toString());
         }
     }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        mediaPlayer.release();
-    }
-
-    protected void startIntentService() {
-        Intent intent = new Intent(this, FetchAddressIntentService.class);
-        intent.putExtra(FetchAddressIntentService.Constants.RECEIVER, mResultReceiver);
-        intent.putExtra(FetchAddressIntentService.Constants.LOCATION_DATA_EXTRA, locationManager);
-        startService(intent);
-    }
-
-    class AddressResultReceiver extends ResultReceiver {
-        String mAddressOutput;
-        public AddressResultReceiver(Handler handler) {
-            super(handler);
-        }
-
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-
-            // Display the address string
-            // or an error message sent from the intent service.
-            mAddressOutput = resultData.getString(FetchAddressIntentService.Constants.RESULT_DATA_KEY);
-
-            // Show a toast message if an address was found.
-            if (resultCode == FetchAddressIntentService.Constants.SUCCESS_RESULT) {
-
-            }
-
-        }
-    }
-
 }
