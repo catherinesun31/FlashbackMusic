502fb4f7c3f88cdb4e792f2d2b7343314fd36457
cailintreseder <ctresede@ucsd.edu>
2018-02-18 21:04:32 -0800

    Hoping to fix errors with crashing

--- app/src/main/java/com/android/flashbackmusicv000/FlashBackMode.java
+++ app/src/main/java/com/android/flashbackmusicv000/FlashBackMode.java
@@ -1,7 +1,12 @@
 package com.android.flashbackmusicv000;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by cailintreseder on 2/18/18.
@@ -20,6 +25,21 @@ public class FlashBackMode {
     }
     public void createQueue() {
         flashQueue.add(unsorted.get(0));
+        for (int i = 1; i < unsorted.size(); ++i) {
+            for (int j = 0; j < flashQueue.size(); ++j) {
 
+            }
+        }
+    }
+
+    public int getScore(Song song) {
+        int total = 0;
+        Calendar calendar = Calendar.getInstance();
+        Date currentTime = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.US);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        //return dateFormat.format(currentTime);
+
+        return total;
     }
 }
