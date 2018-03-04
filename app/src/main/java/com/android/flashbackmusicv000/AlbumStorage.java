0d25b6852182e1b18f180607fed9049285678091
cchallac <cchallac@ucsd.edu>
2018-03-03 20:20:26 -0800

    mostly refactored, just need to fix album storage dependency

--- app/src/main/java/com/android/flashbackmusicv000/AlbumStorage.java
+++ app/src/main/java/com/android/flashbackmusicv000/AlbumStorage.java
@@ -1,5 +1,11 @@
 package com.android.flashbackmusicv000;
 
+import android.app.Activity;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.util.Log;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
@@ -18,13 +24,56 @@ public class AlbumStorage {
         }
     }
 
-    public void initalizeAlbum(){
+    public void initalizeAlbum(Activity a){
+        Field[] fields = R.raw.class.getFields();
+        Song[] songs = new Song[fields.length];
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
 
+        for (int i = 0; i < fields.length; ++i) {
 
+            String path = "android.resource://" + a.getPackageName() + "/raw/" + fields[i].getName();
+            final Uri uri = Uri.parse(path);
+
+            mmr.setDataSource(a.getApplication(), uri);
+
+            // Janice add in: wanted to pass in the file location as Song variable
+            int songId = a.getResources().getIdentifier(fields[i].getName(), "raw", a.getPackageName());
+
+            String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            String artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            String albumName = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+            String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+            long mil = Long.parseLong(duration);
+            int seconds = (int) Math.ceil((mil / 1000) % 60);
+            int minutes = (int) Math.ceil((mil / (1000 * 60)) % 60);
+            duration = minutes + ":" + seconds;
+
+            Log.d("Information: ", "Title: " + title + "\n" +
+                    "Artist: " + artist + "\n" +
+                    "com.android.flashbackmusicv000.Album: " + albumName + "\n" +
+                    "Duration: " + duration);
+            if(!checkAlbum(albumName)){
+
+                albumsList.add(new Album(albumName, currentSong));
+
+            }
+            else {
+                Album albumToAddSong = retrieveAlbum(albumName);
+                albumToAddSong.addSong(new Song(title, songId));
+            }
+            if(i == 0) {
+                this.allSongs = new Album("All Songs From Main Activity",currentSong);
+            }
+            else {
+                this.allSongs.addSong(currentSong);
+            }
+
+        }
 
     }
 
-    private boolean checkAlbum(String albumName){
+    public boolean checkAlbum(String albumName){
 
         for(Album album: albumsList){
             if(album.getName().equals(albumName)){
@@ -34,7 +83,7 @@ public class AlbumStorage {
         return false;
     }
 
-    private Album retrieveAlbum(String albumName){
+    public Album retrieveAlbum(String albumName){
         int index = 0;
         Album currentAlbum = null;
         ListIterator<Album> it = albumsList.listIterator();
