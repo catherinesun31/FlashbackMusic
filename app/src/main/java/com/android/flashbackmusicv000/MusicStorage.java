c02a3c6605dc6589e4ca8e211ad041ad75337e8a
cailintreseder <treseder.cailin@gmail.com>
2018-03-07 11:20:41 -0800

    Update MusicStorage.java

--- app/src/main/java/com/android/flashbackmusicv000/MusicStorage.java
+++ app/src/main/java/com/android/flashbackmusicv000/MusicStorage.java
@@ -29,10 +29,6 @@ public class MusicStorage {
         as.initalizeAlbum();
 
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> e39e543f4eb66a0975feb380b26045d973365e69
 
     public SongStorage getSongStorage(){
         return ss;
