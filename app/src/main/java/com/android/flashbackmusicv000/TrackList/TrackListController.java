466dc366f108b4767f61811be14542bfc8c251e1
James Rich <jcrich@ucsd.edu>
2018-03-10 16:06:34 -0800

    setup Button listeners for the view in content_sng_list.xml

---
+++ app/src/main/java/com/android/flashbackmusicv000/TrackList/TrackListController.java
@@ -0,0 +1,80 @@
+package com.android.flashbackmusicv000.TrackList;
+
+//The View is a fragment
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.android.flashbackmusicv000.R;
+import com.android.flashbackmusicv000.SongPlayingActivity;
+
+/**
+ * Created by MobileComputerWizard on 3/10/2018.
+ */
+
+public class TrackListController extends Fragment {
+//MVC design pattern
+    private View view;
+    //Model model;
+    private Button titleButton;
+    private Button artistButton;
+    private Button albumButton;
+    private Button favouriteButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.content_song_playing, container, false);
+        setView();
+        setModel();
+        return view;
+    }
+
+    private void setView(){
+
+        titleButton = (Button) view.findViewById(R.id.titleButton);
+        artistButton = (Button) view.findViewById(R.id.artistButton);
+        albumButton = (Button) view.findViewById(R.id.albumButton);
+        favouriteButton = (Button) view.findViewById(R.id.favoriteButton);
+        setButtonListener(new SortButtonListener());
+
+    }
+
+    private void setButtonListener(SortButtonListener sortListener){
+
+        titleButton.setOnClickListener(sortListener);
+        artistButton.setOnClickListener(sortListener);
+        albumButton.setOnClickListener(sortListener);
+        favouriteButton.setOnClickListener(sortListener);
+
+    }
+
+    private void setModel(){
+
+
+
+    }
+
+    private class SortButtonListener implements View.OnClickListener {
+
+        public void onClick(View v){
+
+            switch(v.getId()){
+
+                //querying and updating the model, model to handle database seperately
+                //send data from input to the model,
+                /*
+                case R.id.titleButton:  ;break;
+                case R.id.artistButton:  ;break;
+                case R.id.albumButton:  ;break;
+                case R.id.favoriteButton:  ;break;
+                */
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/android/flashbackmusicv000/TrackList/TrackListSorter.java
@@ -0,0 +1,11 @@
+package com.android.flashbackmusicv000.TrackList;
+
+/**
+ * Created by MobileComputerWizard on 3/10/2018.
+ */
+
+public abstract class TrackListSorter {
+
+    //just send diferent track lists????????
+
+}
