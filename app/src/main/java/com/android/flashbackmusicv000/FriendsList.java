060a854955e749f9c17eeb4a952ca10a45151b79
cchallac <cchallac@ucsd.edu>
2018-03-12 11:28:45 -0700

    Adding friendslist using people API

--- app/src/main/java/com/android/flashbackmusicv000/FriendsList.java
+++ app/src/main/java/com/android/flashbackmusicv000/FriendsList.java
@@ -29,9 +29,55 @@ import com.android.flashbackmusicv000.User;
 import com.android.flashbackmusicv000.UserAdapter;
 
 /**
- * Created by fluffy on 3/9/18.
+ * Created by Chelsea 3/9/18.
  */
 
 public class FriendsList extends Activity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, ResultCallback<People.LoadPeopleResult>{
+
+    private GoogleApiClient mGAC = null;
+    private static final String TAG = "Friendslist"
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.SignInActivity);
+        mGAC = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN).addScope(Plus.SCOPE_PLUS_PROFILE).build();
+    }
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart called");
+        mGAC.connect();
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop called");
+
+        if(mGAC.isConnected()){
+            mGAC.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+        Log.d(TAG, "onConnection called");
+
+        Plus.PeopleApi.loadVisible(mGAC, null).setResultCallback(this);
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.d(TAG, "onConnectionFailed called");
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Log.d(TAG, "onConnectionSuspended called");
+    }
+
 }
